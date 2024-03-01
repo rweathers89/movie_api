0eea4359101784672215472e88c85083c033766f
rweathers89 <146603179+rweathers89@users.noreply.github.com>
2024-03-01 06:29:47 -0800

    update app.listen port

--- index.js
+++ index.js
@@ -276,8 +276,9 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-app.listen(8080, () => {
-  console.log('Your app is listening to port 8080.')
+const port = process.env.PORT || 8080;
+ app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on port ' + port)
 });//end app.listen
 
 console.log('My first Node test server is running on Port 8080.');
