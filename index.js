6af8c4ce83b1cd7261b01137a0721a0627b4d575
rweathers89 <146603179+rweathers89@users.noreply.github.com>
2024-04-04 18:48:10 -0700

    added render url to cors

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const cors = require('cors');
 
 //replace app.use(cors()) with code below to ONLY allow CERTAIN origins
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', "https://movie-api-nj6m.onrender.com/"];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
