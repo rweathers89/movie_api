b59dbb69343ab6293d5e4d9a3ba013027569437d
rweathers89 <146603179+rweathers89@users.noreply.github.com>
2024-04-25 17:25:12 -0700

    added passport.authentication
    added passport.authentication after testing /movies endpoint

--- index.js
+++ index.js
@@ -222,9 +222,7 @@ app.delete('/users/:Username/movies/:MovieID',
 
 //MOVIES info
 //READ -- get all movies
-app.get('/movies',
-  // add back after testing endpoint >>> 
-  // , passport.authenticate('jwt', { session: false })
+app.get('/movies', passport.authenticate('jwt', { session: false }),
   async (req, res) => {
     await Movies.find()
       .then((movies) => {
